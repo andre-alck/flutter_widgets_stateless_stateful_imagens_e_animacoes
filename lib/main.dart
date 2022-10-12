cb362e874fa11282f741e2d0897ef475154f7d58
andre-alck <andrealck1@gmail.com>
2022-10-12 19:47:07 -0300

    feat: add Faça como eu fiz: o Widget contra ataca

--- lib/main.dart
+++ lib/main.dart
@@ -7,129 +7,95 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: Scaffold(
         appBar: AppBar(
+          leading: const Icon(
+            Icons.add_task,
+          ),
           title: const Text(
-            'Tarefas',
+            'Flutter: Primeiros Passos',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
-        body: ListView(
+        backgroundColor: Colors.white60,
+        body: Column(
           children: const [
-            Task(
-              'Learn Flutter',
+            Model(
+              [
+                Colors.white,
+                Colors.pink,
+                Colors.blue,
+              ],
             ),
-            Task(
-              'Ride a bike',
+            Model(
+              [
+                Colors.pink,
+                Colors.purple,
+                Colors.blue,
+              ],
             ),
-            Task(
-              'Meditate',
+            Model(
+              [
+                Colors.pink,
+                Colors.orange,
+                Colors.blue,
+              ],
+            ),
+            Model(
+              [
+                Colors.purple,
+                Colors.white,
+                Colors.green,
+              ],
             ),
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
         ),
       ),
     );
   }
 }
 
-class Task extends StatefulWidget {
-  final String name;
-  const Task(
-    this.name, {
+class Model extends StatelessWidget {
+  final List<Color> colors;
+
+  const Model(
+    this.colors, {
     super.key,
   });
 
   @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  double level = 0;
-
-  @override
-  Widget build(
-    BuildContext context,
-  ) {
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(
-        8.0,
+        10,
       ),
-      child: Stack(
+      child: Row(
         children: [
           Container(
-            color: Colors.blue,
-            height: 140,
+            height: 150,
+            width: 100,
+            color: colors[0],
           ),
-          Column(
-            children: [
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
-                    SizedBox(
-                      width: 200,
-                      child: Text(
-                        widget.name,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => setState(
-                        () => level++,
-                      ),
-                      child: const Icon(
-                        Icons.arrow_drop_up,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(
-                  8.0,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 200,
-                      child: LinearProgressIndicator(
-                        value: level / 10,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'Nível: $level',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          Container(
+            height: 150,
+            width: 100,
+            color: colors[1],
+          ),
+          Container(
+            height: 150,
+            width: 100,
+            color: colors[2],
           ),
         ],
       ),
